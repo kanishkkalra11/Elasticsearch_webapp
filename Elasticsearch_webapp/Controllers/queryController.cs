fed5a69de331a43a3dc38c50f8ae2c36b8a05325
khal_kanishk11 <kanishkkalra1999@gmail.com>
2021-02-26 15:48:40 +0530

    Initial Commit

---
+++ Elasticsearch_webapp/Controllers/queryController.cs
@@ -0,0 +1,66 @@
+﻿using Elasticsearch_webapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch_webapp.Controllers
+{
+    public class queryController : Controller
+    {
+        private readonly ElasticClient _client;
+
+        public queryController(ElasticClient client)
+        {
+            _client = client;
+        }
+
+
+
+        [HttpPost]
+        public IActionResult display(query m)
+        {
+
+            string sim = m.content;
+            ISearchResponse<Book> results;
+
+            if (!string.IsNullOrWhiteSpace(sim))
+            {
+                results = _client.Search<Book>(s => s
+                    .Query(q => q
+                        .Match(t => t
+                            .Field(f => f.Title)
+                            .Query(sim)
+                        )
+                    )
+                );
+            }
+            else
+            {
+                results = _client.Search<Book>(s => s
+                   .Query(q => q
+                       .MatchAll()
+                   )
+                   .Aggregations(a => a
+                       .Range("pageCounts", r => r
+                           .Field(f => f.PageCount)
+                           .Ranges(r => r.From(0),
+                                   r => r.From(200).To(400),
+                                   r => r.From(400).To(600),
+                                   r => r.From(600)
+                           )
+                       )
+                       .Terms("categories", t => t
+                           .Field("categories.keyword")
+                       )
+                   )
+               );
+            }
+            return View(results);
+
+        }
+    }
+}
---
+++ Elasticsearch_webapp/Models/query.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elasticsearch_webapp.Models
+{
+    public class query
+    {
+        public string content { get; set; }
+    }
+}
